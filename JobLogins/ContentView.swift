29097d3fede14a358d0d89bf3113564a0e9c43bf
“Praks” <prakshapan@gmail.com>
2020-07-15 06:54:23 +0545

    Initial Commit

---
+++ JobLogins/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  JobLogins
+//
+//  Created by Praks on 7/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JobLogins/JobLoginsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobLoginsApp.swift
+//  JobLogins
+//
+//  Created by Praks on 7/15/20.
+//
+
+import SwiftUI
+
+@main
+struct JobLoginsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
